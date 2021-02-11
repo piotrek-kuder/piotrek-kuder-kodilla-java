41d6b9e0c71c921a784fb8bc24801607439950bf
pk <abazel@localhost.localdomain>
2021-02-11 22:00:53 +0100

    6.2 adding submodules 'calculator' and 'user' to com.kodilla.testing + test in main method

--- kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
@@ -1,7 +1,34 @@
 package com.kodilla.testing;
 
+import com.kodilla.testing.calculator.Calculator;
+import com.kodilla.testing.user.SimpleUser;
+
 public class TestingMain {
     public static void main(String[] args) {
-        System.out.println("Moduł 6. Wprowadzenie to testowania oprogramowania");
+        SimpleUser simpleUser = new SimpleUser("theForumUser");
+
+        String result = simpleUser.getUsername();
+
+        if (result.equals("theForumUser")) {
+            System.out.println("test OK");
+        } else {
+            System.out.println("Error!");
+        }
+
+        System.out.println("Test - pierwszy test jednostkowy:");
+
+        Calculator calculator = new Calculator();
+
+        if(calculator.add(55, -13)==(55 + (- 13))) {
+            System.out.println("Testing \"add\" method: OK");
+        } else {
+            System.out.println("Testing \"add\" method: ERROR");
+        }
+
+        if(calculator.subtract(55, -13)==(55 - (- 13))) {
+            System.out.println("Testing \"subtract\" method: OK");
+        } else {
+            System.out.println("Testing \"subtract\" method: ERROR");
+        }
     }
 }
