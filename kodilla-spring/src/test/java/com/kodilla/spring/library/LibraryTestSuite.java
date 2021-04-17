8ffb5bf3514c33ae92d14738f66ba65cd5d3b82d
pk <abazel@localhost.localdomain>
2021-04-17 19:39:18 +0200

    adding package com.kodilla.spring.library

--- kodilla-spring/src/test/java/com/kodilla/spring/library/LibraryTestSuite.java
+++ kodilla-spring/src/test/java/com/kodilla/spring/library/LibraryTestSuite.java
@@ -1,20 +1,20 @@
 package com.kodilla.spring.library;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+import java.util.Arrays;
 
 @SpringBootTest
 public class LibraryTestSuite {
 
-    @Autowired
-    private Library library;
-
-    @Test
+        @Test
     void testLoadFronDb() {
         //Given
-        //ApplicationContext context = new AnnotationConfigApplicationContext("com.kodilla.spring");
-        //Library library = context.getBean(Library.class);
+        ApplicationContext context = new AnnotationConfigApplicationContext("com.kodilla.spring");
+        Library library = context.getBean(Library.class);
         //When
         library.loadFromDb();
         //Then
@@ -24,11 +24,22 @@ public class LibraryTestSuite {
     @Test
     void testSaveFromDb() {
         //Given
-        //ApplicationContext context = new AnnotationConfigApplicationContext("com.kodilla.spring");
-        //Library library = context.getBean(Library.class);
+        ApplicationContext context = new AnnotationConfigApplicationContext("com.kodilla.spring");
+        Library library = context.getBean(Library.class);
         //When
         library.saveToDb();
         //Then
         //nothing to do
+    }
+
+    @Test
+    void testContext() {
+        //Given
+        ApplicationContext context = new AnnotationConfigApplicationContext(LibraryConfig.class);
+        //When & Then
+        System.out.println("==== Beans list: === >>>");
+        Arrays.stream(context.getBeanDefinitionNames())
+                .forEach(System.out::println);
+        System.out.println("<<< ==== Beans list: ===");
     }
 }
