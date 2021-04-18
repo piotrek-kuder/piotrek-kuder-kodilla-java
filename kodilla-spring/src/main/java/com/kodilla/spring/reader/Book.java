16f597f3fd0808a50645789682fc49afd759cbeb
pk <abazel@localhost.localdomain>
2021-04-18 07:25:48 +0200

    14.4 adding Spring Web

--- kodilla-spring/src/main/java/com/kodilla/spring/reader/Book.java
+++ kodilla-spring/src/main/java/com/kodilla/spring/reader/Book.java
@@ -1,2 +1,14 @@
-package com.kodilla.spring.reader;public class Book {
+package com.kodilla.spring.reader;
+
+public class Book {
+
+    private final String title;
+
+    public Book(String title) {
+        this.title = title;
+    }
+
+    public String getTitle() {
+        return title;
+    }
 }
--- kodilla-spring/src/main/java/com/kodilla/spring/reader/Reader.java
+++ kodilla-spring/src/main/java/com/kodilla/spring/reader/Reader.java
@@ -1,2 +1,14 @@
-package com.kodilla.spring.reader;public class Reader {
+package com.kodilla.spring.reader;
+
+public class Reader {
+
+    private final Book theBook;
+
+    public Reader(Book theBook) {
+        this.theBook = theBook;
+    }
+
+    public void read() {
+        System.out.println("Reading:  " + theBook.getTitle());
+    }
 }
