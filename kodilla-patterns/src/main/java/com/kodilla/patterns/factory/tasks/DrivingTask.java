768c5bbb1461ac5afb053801fb30bbf77181aa7f
pk <abazel@localhost.localdomain>
2021-04-24 12:54:54 +0200

    15.4 adding package com.kodilla.patterns.factory

---
+++ kodilla-patterns/src/main/java/com/kodilla/patterns/factory/tasks/DrivingTask.java
@@ -0,0 +1,37 @@
+package com.kodilla.patterns.factory.tasks;
+
+public class DrivingTask implements Task {
+    private final String taskName;
+    private final String where;
+    private final String using;
+    private boolean isExecuted = false;
+
+    public DrivingTask(String taskName, String where, String using) {
+        this.taskName = taskName;
+        this.where = where;
+        this.using = using;
+    }
+
+    @Override
+    public void executeTask() {
+        this.isExecuted = true;
+    }
+
+    @Override
+    public String getTaskName() {
+        return taskName;
+    }
+
+    @Override
+    public boolean isTaskExecuted() {
+        return isExecuted;
+    }
+
+    public String getWhere() {
+        return where;
+    }
+
+    public String getUsing() {
+        return using;
+    }
+}
---
+++ kodilla-patterns/src/main/java/com/kodilla/patterns/factory/tasks/PaintingTask.java
@@ -0,0 +1,37 @@
+package com.kodilla.patterns.factory.tasks;
+
+public class PaintingTask implements Task {
+    private final String taskName;
+    private final String color;
+    private final String whatToPaint;
+    private boolean isExecuted = false;
+
+    public PaintingTask(final String taskName, final String color, final String whatToPaint) {
+        this.taskName = taskName;
+        this.color = color;
+        this.whatToPaint = whatToPaint;
+    }
+
+    @Override
+    public void executeTask() {
+        this.isExecuted = true;
+    }
+
+    @Override
+    public String getTaskName() {
+        return taskName;
+    }
+
+    @Override
+    public boolean isTaskExecuted() {
+        return isExecuted;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public String getWhatToPaint() {
+        return whatToPaint;
+    }
+}
